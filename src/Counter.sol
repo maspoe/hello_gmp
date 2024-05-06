74a16066d37ea21ee006ddb2ab385972f8ae1f63
maspoe <kontakmaspoe@gmail.com>
2024-05-06 21:38:48 +0700

    Import

--- src/Counter.sol
+++ src/Counter.sol
@@ -17,4 +17,22 @@ contract Counter is IGmpReceiver {
         number++;
         return bytes32(number);
     }
+
+    import {GmpTestTools} from "@analog-gmp-testing/GmpTestTools.sol";
+
+// Deploy gateway contracts and create forks for all supported networks
+GmpTestTools.setup();
+
+// Set `account` balance in all networks
+GmpTestTools.deal(address(account), 100 ether);
+
+// Switch to Sepolia network
+GmpTestTools.switchNetwork(5);
+
+// Switch to Shibuya network and set `account` as `msg.sender` and `tx.origin`
+GmpTestTools.switchNetwork(7, address(account));
+
+// Relay all pending GMP messages.
+GmpTestTools.relayMessages();
+
 }
